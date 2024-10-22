a157e3c30deb960c3e2622c32ff920fac01aa3e4
inprovert <92786046+inprovert@users.noreply.github.com>
2024-10-22 23:56:50 +0530

    binary search

---
+++ src/BinarySearch.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList <String> strArr = new ArrayList<>();
+
+        for(int a = 0 ; a<5 ;a++){
+            strArr.add(sc.nextLine());
+        }
+        System.out.println(strArr);
+        System.out.print("enter search string: ");
+        String searchStr = sc.nextLine();
+
+        int index = -1 ;
+
+        for ( String str : strArr){
+            if( str.equals(searchStr)){
+                index = strArr.indexOf(str);
+                break;
+            }
+
+        }
+
+        System.out.println(index);
+    }
+}
